03115555337f2d6806468becf4d621361241c359
Liam-Rauch576 <Rauchliam@yahoo.com>
2026-08-04 16:43:54 -0400

    Revert "RouteRunning"
    This reverts commit fb94f9fff6956bf3734c2bf8b3fc8390f9ade9aa.

--- Assets/Game Stuff/MovementSystem/WideReceiver.cs
+++ Assets/Game Stuff/MovementSystem/WideReceiver.cs
@@ -1,67 +1,10 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using System.Collections.Generic;
-
-public enum ReceiverState
-{
-    PreSnap,
-    RunningRoute,
-    Scrambling,
-    Catching,
-    Blocking,
-
-}
 
 public class WideReceiver : Player
 {
-    public ReceiverState ReceiverState { get; private set; } = ReceiverState.PreSnap;
 
-    private float catching;
-    private float blocking;
-    private float maxSpeed = 5f;
+    public float catching;
+    public float blocking;
 
-    [SerializeField] private float waypointTolerance = 0.15f;
-
-    private Vector3[] worldWaypoints;
-    private int currentIndex;
-
-    public bool isRightOfCenter;
-
-    // Call this once, at the moment the route should start.
-    public void StartRoute(Vector2[] offsets, Vector3 snapSpot, Vector3 fieldRight, Vector3 fieldForward)
-    {
-        worldWaypoints = new Vector3[offsets.Length];
-        for (int i = 0; i < offsets.Length; i++)
-        {
-            float lateral = isRightOfCenter ? -offsets[i].x : offsets[i].x;
-            worldWaypoints[i] = snapSpot + fieldRight * lateral + fieldForward * offsets[i].y;
-        }
-
-        currentIndex = 0;
-        ReceiverState = ReceiverState.RunningRoute;
-    }
-
-    private void Update()
-    {
-        if (ReceiverState == ReceiverState.RunningRoute)
-        {
-            RunTowardCurrentWaypoint();
-        }
-    }
-
-    private void RunTowardCurrentWaypoint()
-    {
-        Vector3 target = worldWaypoints[currentIndex];
-        float trueSpeed = (speed / 100f) * maxSpeed;
-        transform.position = Vector3.MoveTowards(transform.position, target, trueSpeed * Time.deltaTime);
-
-        if (Vector3.Distance(transform.position, target) <= waypointTolerance)
-        {
-            currentIndex++;
-            if (currentIndex >= worldWaypoints.Length)
-            {
-                ReceiverState = ReceiverState.Scrambling; // route's over, nothing left to run
-            }
-        }
-    }
 }
